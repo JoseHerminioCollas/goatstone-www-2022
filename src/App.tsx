b658b3441149057ad5e8b17c3435c05425b4edf7
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2023-05-12 12:36:56 -0400

    Delete entries

--- src/App.tsx
+++ src/App.tsx
@@ -122,23 +122,6 @@ function App() {
         </a>
         <img src="img/2d-data-screen-shot.png" width="400" />
       </article>
-
-      <article>
-        <h3>DataLook</h3>
-        <a href="https://goatstone.com/dl/" target="new">
-          https://goatstone.com/dl/
-        </a>
-        <p>
-          The display of data in a reactive layout.
-          The application uses TypeScript and RxJS patterns for combining data with style definitions.
-          The code can be found in the Github repository.
-        </p>
-        Git Hub :
-        <a href="https://github.com/JoseHerminioCollas/data-view" target="new">
-          https://github.com/JoseHerminioCollas/data-view
-        </a>
-        <img src="img/data-look-screen.png" width="400" />
-      </article>
       <article>
         <h3>
           View GIBS&nbsp;
@@ -152,58 +135,8 @@ function App() {
         </p>
         <img src="img/view-gibs-app-sml.png" />
       </article>
-      <article>
-        <h3>
-          Ameb, a game
-          &nbsp;
-          <a href="http://ameb.goatstone.com/" target="new">http://ameb.goatstone.com/</a>
-        </h3>
-        <p>
-          <img src="img/ameb.png" alt="" />
-          Ameb was an entry into the JS13kGames JavaScript competition. Ameb must eat food in order to accumulate
-          health points and stay alive.
-          The player of Ameb navigates the character, Ameb, to catch food.
-        </p>
-      </article>
-      <article>
-        <h3>
-          MapLab
-          &nbsp;
-          <a href="https://map-lab-goatstone.appspot.com" target="new">https://map-lab-goatstone.appspot.com</a>
-        </h3>
-        <p>
-          Experiments with mapping libraries:
-          &nbsp;
-          <a href="https://cloud.google.com/maps-platform/">
-            Google
-          </a>,
-          &nbsp;
-          <a href="https://leafletjs.com/">
-            Leaflet
-          </a> and
-          &nbsp;
-          <a href="https://www.bingmapsportal.com/">
-            Bing
-          </a>
-          .
-        </p>
-        <img src="img/map-lab.png" />
-      </article>
-      <article>
-        <h3>
-          D3 Framework
-          &nbsp;<a href="https://d3-frame.goatstone.com" target="new">https://d3-frame.goatstone.com</a>
-        </h3>
-        <p>
-          <img src="img/d3-frame-1.png" className="right" width="250" height="160" alt="" />
-          <img src="img/d3-frame-3.png" className="right" width="250" height="160" alt="" /> D3 and React
-          Methodologies for using the D3 visualization library with React. I have been working with React
-          ( reactjs.org ) and D3 ( d3js.org ) together, and I am documenting methodologies in a GitHub repository.
-          <a href="https://github.com/goatstone/d3-frame" target="new">https://github.com/goatstone/d3-frame</a>
-        </p>
-      </article>
       <footer>
-        Goatstone &copy; 2022
+        Goatstone &copy; 2023
       </footer>
     </>
   );
